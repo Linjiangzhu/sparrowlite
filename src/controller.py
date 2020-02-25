79f2c71a1714f16fa49c7c52e6a09b00e4f6c5aa
Linjiangzhu <yuyuex97@gmail.com>
2020-02-24 23:42:19 -0800

    turn dummy result file absolute path to reletive path

--- src/controller.py
+++ src/controller.py
@@ -3,9 +3,9 @@ from model import PresentPage, FileProcesser
 
 def get_dummy_result() -> list:
     dummy = [
-        "/Users/user/python-project/sparrowlite/ANALYST/www_cs_uci_edu/ff6386a56527801874c4fa418c3467f6062dcc82c0ea7dbcb5d5b773e847e05b.json",
-        "/Users/user/python-project/sparrowlite/ANALYST/www_cs_uci_edu/fb25ac3aea8ba552b55c822a8fef95a8029aba51d0188c5c4f461cb472e0f489.json",
-        "/Users/user/python-project/sparrowlite/ANALYST/www_cs_uci_edu/e5f576c31e66e42fb83ab6144d8f7c74e25bc11a94a76f3af66cfa41208f9be9.json"
+        "../ANALYST/www_cs_uci_edu/ff6386a56527801874c4fa418c3467f6062dcc82c0ea7dbcb5d5b773e847e05b.json",
+        "../ANALYST/www_cs_uci_edu/fb25ac3aea8ba552b55c822a8fef95a8029aba51d0188c5c4f461cb472e0f489.json",
+        "../ANALYST/www_cs_uci_edu/e5f576c31e66e42fb83ab6144d8f7c74e25bc11a94a76f3af66cfa41208f9be9.json"
     ]
     result = []
     freader = FileProcesser()
